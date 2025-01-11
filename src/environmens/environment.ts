add417eb38ab7100d9cfdaa223105f6119bb6654
fazliddin <fazliddinnarkhajayev@gmail.com>
2025-01-11 18:05:37 +0500

    add

--- src/environmens/environment.ts
+++ src/environmens/environment.ts
@@ -17,9 +17,9 @@
 
 export const env = {
   production: false,
-  apiUrl: 'https://api.tirgo.io/api/v2',
-  adminUrl: 'https://api.tirgo.io/api/v2',
-  references: 'https://api.tirgo.io/api/v2',
-  merchantUrl: 'https://api.tirgo.io/api/v2',
-  orderApiUrl: 'https://api.tirgo.io/api/v2',
+  apiUrl: 'https://test-api.tirgo.io/api/v2',
+  adminUrl: 'https://test-api.tirgo.io/api/v2',
+  references: 'https://test-api.tirgo.io/api/v2',
+  merchantUrl: 'https://test-api.tirgo.io/api/v2',
+  orderApiUrl: 'https://test-api.tirgo.io/api/v2',
 };
