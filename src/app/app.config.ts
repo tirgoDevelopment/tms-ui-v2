047f81cd871f1a8f5d197e7b58e8d25a14fd9400
Najmiddin <najmiddinshonasriddinov@gmail.com>
2025-01-13 17:54:21 +0500

    driver list

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -49,9 +49,5 @@ export const appConfig: ApplicationConfig = {
         }
     })),
     { provide: NZ_I18N, useValue: ru_RU },
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })
 ],
 };
