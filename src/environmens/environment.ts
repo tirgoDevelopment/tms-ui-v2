0cf7a3085b4ba19b789de9ae264838714bc3461d
Najmiddin <najmiddinshonasriddinov@gmail.com>
2025-01-21 13:37:11 +0500

    last test

--- src/environmens/environment.ts
+++ src/environmens/environment.ts
@@ -1,11 +1,11 @@
-// export const env = {
-//   production: false,
-//   apiUrl: 'https://test-api.tirgo.io/api/v2',
-//   adminUrl: 'https://test-api.tirgo.io/api/v2',
-//   references: 'https://test-api.tirgo.io/api/v2',
-//   merchantUrl: 'https://test-api.tirgo.io/api/v2',
-//   orderApiUrl: 'https://test-api.tirgo.io/api/v2',
-// };
+export const env = {
+  production: false,
+  apiUrl: 'https://test-api.tirgo.io/api/v2',
+  adminUrl: 'https://test-api.tirgo.io/api/v2',
+  references: 'https://test-api.tirgo.io/api/v2',
+  merchantUrl: 'https://test-api.tirgo.io/api/v2',
+  orderApiUrl: 'https://test-api.tirgo.io/api/v2',
+};
 // export const env = {
 //   production: false,
 //   apiUrl: 'http://192.168.1.218:3009/api/v2',
@@ -15,11 +15,11 @@
 //   orderApiUrl: 'http://192.168.1.218:3001/api/v2',
 // };
 
-export const env = {
-  production: false,
-  apiUrl: 'https://api.tirgo.io/api/v2',
-  adminUrl: 'https://api.tirgo.io/api/v2',
-  references: 'https://api.tirgo.io/api/v2',
-  merchantUrl: 'https://api.tirgo.io/api/v2',
-  orderApiUrl: 'https://api.tirgo.io/api/v2',
-};
+// export const env = {
+//   production: false,
+//   apiUrl: 'https://api.tirgo.io/api/v2',
+//   adminUrl: 'https://api.tirgo.io/api/v2',
+//   references: 'https://api.tirgo.io/api/v2',
+//   merchantUrl: 'https://api.tirgo.io/api/v2',
+//   orderApiUrl: 'https://api.tirgo.io/api/v2',
+// };
